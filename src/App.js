ec99451a66e2fd8548435ddad52048207d5114a3
Shaxadhere <Shaxad.here@gmail.com>
2021-03-26 15:55:01 +0500

    axios call success

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React, {Component} from 'react'
 import axios from 'axios'
 
-class App extends Component{
-  constructor(props){
-    super(props)
-    //creating state
-    this.state = {
-      users : []
+class App extends Component {
+    constructor(props) {
+        super(props)
+        // creating state
+        this.state = {
+            users: [],
+            loading: false
+        }
     }
-  }
 
-  componentWillMount(){
-    axios('http://localhost:8080/')
-    .then(response => this.setState())
-  }
+    getUsers(){
+      this.setState()
+      axios('http://localhost:8080/').then(response => this.setState({users: response.data.posts}))
+    }
 
-  render(){
-    return <div className="App">Hello</div>
-  }
+    componentWillMount() {
+      this.getUsers()
+    }
+
+    render() {
+        return <div className="App">
+            {
+            this.state.users.map(user => <div>{
+                user.title
+            }</div>)
+        } </div>
+    }
 }
 
 export default App;
