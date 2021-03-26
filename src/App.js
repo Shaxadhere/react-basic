5276067fc186ebf7b27430fa9ed5f93230fd1969
Shaxadhere <Shaxad.here@gmail.com>
2021-03-26 15:38:41 +0500

    axios call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 class App extends Component{
+  constructor(props){
+    super(props)
+    //creating state
+    this.state = {
+      users : []
+    }
+  }
+
+  componentWillMount(){
+    axios('http://localhost:8080/')
+    .then(response => this.setState())
+  }
+
   render(){
     return <div className="App">Hello</div>
   }
